81c28d46758f3babb23534616a6ab501dd5baae2
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-11-06 17:08:25 +0600

    added order page

--- db.sql
+++ db.sql
@@ -29,3 +29,17 @@ CREATE TABLE IF NOT EXISTS `book_send` (
   `price` DECIMAL(10,2) DEFAULT NULL,
   PRIMARY KEY (`sno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+-- Create table with new name
+CREATE TABLE IF NOT EXISTS `book_order` (
+  `sno` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `company` VARCHAR(255) NOT NULL,
+  `book` VARCHAR(255) DEFAULT NULL,
+  `mobile` VARCHAR(20) DEFAULT NULL,
+  `quantity` INT DEFAULT NULL,
+  `price` DECIMAL(10,2) DEFAULT NULL,
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
