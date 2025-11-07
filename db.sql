0b782bc5f1b075809e6c4cf2e8d9ecb0b6470ff5
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-11-07 21:30:08 +0600

    receiver addedpage

--- db.sql
+++ db.sql
@@ -43,3 +43,16 @@ CREATE TABLE IF NOT EXISTS `book_order` (
   `price` DECIMAL(10,2) DEFAULT NULL,
   PRIMARY KEY (`sno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- Create table with new name
+CREATE TABLE IF NOT EXISTS `book_receiver` (
+  `sno` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `company` VARCHAR(255) NOT NULL,
+  `book` VARCHAR(255) DEFAULT NULL,
+  `mobile` VARCHAR(20) DEFAULT NULL,
+  `quantity` INT DEFAULT NULL,
+  `price` DECIMAL(10,2) DEFAULT NULL,
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
